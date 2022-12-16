0cdb6e30727f354c0d696f66132071e7baf2c892
MetelevDM <metelevdm88@gmail.com>
2022-12-16 20:38:23 +0400

    Готово

---
+++ Video_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+if (DateTime.Now.Hour < 12)
+{
+    Console.WriteLine("Доброе утро!");
+}
+else
+{
+    Console.WriteLine("Добрый вечер!");
+}
+Console.WriteLine("Введите, пожалуйста, Ваше имя и нажмите Enter: ");
+string name = Console.ReadLine();
+Console.WriteLine("Введите, пожалуйста, название блюда и нажмите Enter: ");
+string order = Console.ReadLine();
+Console.WriteLine("Спасибо, за Ваш заказ, {0}! Вы выбрали {1}!", name, order);
+int time= new Random().Next(5, 20);
+Console.WriteLine("Ваш заказ будет готов через {0}! минут", time);
